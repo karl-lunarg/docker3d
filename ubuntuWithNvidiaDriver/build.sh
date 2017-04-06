13fdfdbf55169293451fa4282c0a980857d617bf
Karl Schultz <karl@lunarg.com>
2017-04-06 17:04:03 -0600

    Get scripts working on my machine.

--- ubuntuWithNvidiaDriver/build.sh
+++ ubuntuWithNvidiaDriver/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 IMAGE_NAME=ubuntu_with_nvidia_driver
-NVIDIA_DRIVER=NVIDIA-Linux-x86_64-337.12.run  # path to nvidia driver
+NVIDIA_DRIVER=~/drivers/NVIDIA-Linux-x86_64-375.27.08.run  # path to nvidia driver
 
 cp ${NVIDIA_DRIVER} NVIDIA-DRIVER.run
 sudo docker build -t ${IMAGE_NAME} .
